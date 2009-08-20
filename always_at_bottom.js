abc043fe02d3018cb1c6d3b15cf26fbca7908d73
Neeraj Singh <neerajdotname@gmail.com>
2009-08-20 16:06:28 -0400

    variable names referred to wrong value

--- always_at_bottom.js
+++ always_at_bottom.js
@@ -26,11 +26,10 @@
         
         //exteranl css properties are available only on window.onLoad (safari/chrome)
         $(window).load(function(){
-          var footer_margin_top = $this.css('margin-bottom');
           var footer_width = $this.css('width');
-          var footer_margin_top = $this.css('margin-bottom');
-          var footer_margin_top_d = getDigitFromDigitPX(footer_margin_top);
-          var footer_bottom_position_should_be = 0  - footer_margin_top_d;
+          var footer_margin_bottom = $this.css('margin-bottom');
+          var footer_margin_bottom_d = getDigitFromDigitPX(footer_margin_bottom);
+          var footer_bottom_position_should_be = 0  - footer_margin_bottom_d;
           $this.css({position: "absolute", bottom:(footer_bottom_position_should_be + "px"), 'width':footer_width});
         });
 
@@ -49,5 +48,5 @@
 
 
     }); // end of return this.each(function())
-  }; // end of plugin
+  }; // end of pluging
 })(jQuery);
